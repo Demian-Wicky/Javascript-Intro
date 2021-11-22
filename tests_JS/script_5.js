5c209e0af093867c4f1196f6dc4d07ff0dbb0d54
Demian-Wicky <demian.wicky@gmail.com>
2021-11-22 21:02:07 +0100

    some progress

--- tests_JS/script_5.js
+++ tests_JS/script_5.js
@@ -19,3 +19,52 @@ const entrepreneurs = [
     { first: 'Peter', last: 'Thiel', year: 1967 }
 ];
 
+
+// Filtre dans cette liste les entrepreneurs qui sont nés dans les années 70
+entrepreneurs.forEach(element => {
+    if (element.year >= 1970 && element.year <= 1979){
+        console.log(element);
+    }
+});
+
+
+// Sors une array qui contient le prénom et le nom des entrepreneurs
+let res = []
+entrepreneurs.forEach(element => {
+    res.push(element.first + " " + element.last)
+});
+console.log(res);
+
+
+// Quel âge aurait chaque inventeur aujourd'hui
+let today = new Date();
+let currentYear = today.getFullYear();
+
+let result = []
+entrepreneurs.forEach(element => {
+    result.push({first: element.first, last: element.last, age: (currentYear-element.year)})
+});
+console.log(result);
+
+newArray = entrepreneurs
+newArray.forEach(element => {
+    element.age = (currentYear-element.year)
+});
+console.log(newArray);
+
+// Trie les entrepreneurs par ordre alphabétique du nom de famille.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+entrepreneurs.sort(function(a, b) {
+    var nameA = a.last.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.last.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+        return 1;
+    }
+    // names must be equal
+    return 0;
+});
+
+console.log(entrepreneurs);
