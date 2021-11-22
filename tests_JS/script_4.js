a17d81741d9c304123b0385320b3b41c27a3a37f
Demian-Wicky <demian.wicky@gmail.com>
2021-11-22 18:53:28 +0100

    some progress

--- tests_JS/script_4.js
+++ tests_JS/script_4.js
@@ -1,12 +1,9 @@
 function pyramid(n){
     if (n > 0 && Number.isInteger(n) === true ){
-
         for(let count = 1; count <= n; count++){
             console.log(" ".repeat(n - count) + "#".repeat(count));
         }
-
     }
-
     else{
         return "Choix invalide";
     }  
