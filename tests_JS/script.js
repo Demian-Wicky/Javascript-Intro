451ed32ec2fc0b51147162f70009c9c0351588c0
Demian-Wicky <demian.wicky@gmail.com>
2021-11-23 17:44:21 +0100

    some progress

---
+++ tests_JS/script.js
@@ -0,0 +1,17 @@
+let THPSessionNum2 = {
+    numOfMouss: 80,
+    cities: ["Paris", "Lyon", "Montpellier"],
+    successRate: 0.80,
+    sessionMoto: "keep pushing to the top"
+};
+
+console.log(THPSessionNum2.numOfMouss);
+console.log(THPSessionNum2.sessionMoto);
+
+THPSessionNum2.numOfMouss = 79; // je modifie un attribut existant
+console.log(THPSessionNum2.numOfMouss);
+THPSessionNum2.favoriteLesson = "Présentation de Sinatra" //je rajoute un attribut
+console.log(THPSessionNum2); //j'imprime tout l'objet pour voir
+
+let attributName = "successRate";
+console.log(THPSessionNum2[attributName]);
--- tests_JS/script_6.js
+++ tests_JS/script_6.js
@@ -1,6 +1,6 @@
 const books = [
     { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
-    { title: 'A la recherche du temps perdu', id: 237634, rented: 28 },
+    { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
     { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
     { title: 'Les frères Karamazov', id: 450911, rented: 55 },
     { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
@@ -8,38 +8,40 @@ const books = [
     { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
     { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
     { title: 'La disparition', id: 364445, rented: 33 },
-    { title: 'La lune seule le sait', id: 63541, rented: 9 },
+    { title: 'La lune seule le sait', id: 63541, rented: 43 },
     { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
     { title: 'Guerre et Paix', id: 748147, rented: 19 }
 ];
 
 
 // Est-ce que tous les livres ont été au moins empruntés une fois ?
+console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?")
+
 function untouchedBookDetector(booksList){
-    let output = false;
+    let output = "oui";
     booksList.every(book => {
         if (book.rented == 0){
-            return output = true
+            return output = "non"
         }
     })
     return output;
 };
 
-let result = untouchedBookDetector(books)
-console.log(result)
+let result1 = untouchedBookDetector(books)
+console.log(result1)
 
 
 
+// Quel est le livre le plus emprunté ?
+console.log("Quel est le livre le plus emprunté ?")
 
-
-// Quel est livre le plus emprunté ?
 function mostRentedBook(booklist){
     let orderedList = 
     booklist.sort(function (a, b) {
         return a.rented - b.rented
     });
 
-    return orderedList.pop()
+    return orderedList.slice(-1)[0]
 }
 
 let result2 = mostRentedBook(books)
@@ -47,9 +49,9 @@ console.log(result2);
 
 
 
-
-
 // Quel est le livre le moins emprunté ?
+console.log("Quel est le livre le moins emprunté ?")
+
 function leastRentedBook(booklist){
     let orderedList = 
     booklist.sort(function (a, b) {
@@ -64,12 +66,11 @@ console.log(result3)
 
 
 
-
-
 // Trouve le livre avec l'ID: 873495 ;
-function findBookWithID(bookList, ID){
-    let output = bookList.find( book => book.id == ID);
-    return output
+console.log("Trouve le livre avec l'ID: 873495")
+
+function findBookWithID(bookList, numID){
+    return bookList.find( book => book.id == numID);
 }
 
 let result4 = findBookWithID(books, 873495)
